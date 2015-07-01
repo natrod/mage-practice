3db4169e59dd7667a5b87273d895ac581faaee31
Nathaniel Rodrigues <nathaniel.theodore@borngroup.com>
2015-07-01 13:01:10 +0530

    Added Class details

--- app/code/local/Nrod/Giftregistry/Model/Mysql4/Entity.php
+++ app/code/local/Nrod/Giftregistry/Model/Mysql4/Entity.php
@@ -6,3 +6,10 @@
  * and open the template in the editor.
  */
 
+class Nrod_Giftregistry_Model_Mysql4_Entity extends Mage_Core_Model_Mysql4_Abstract
+{
+    public function _construct()
+    {
+        $this->_init('giftregistry/item','entity_id');
+    }
+}
